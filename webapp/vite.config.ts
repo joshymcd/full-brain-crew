aeba4b554be5321c76d1616f948d76fb4b30449e
Josh McDonald <joshymcd92@gmail.com>
2026-06-21 18:31:05 +0100

    setup base webapp

---
+++ webapp/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite-plus";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+
+const config = defineConfig({
+  fmt: {},
+  lint: { options: { typeAware: true, typeCheck: true } },
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
